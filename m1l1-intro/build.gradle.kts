f3ff047407c9ad810485fad7b35403dbc69ed62f
apisarevskiy <apisarevskiy@yandex.ru>
2022-03-09 17:42:08 +0300

    Initial commit

---
+++ m1l1-intro/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "ru.otus.otuskotlin.marketplace"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(kotlin("test-junit"))
+
+
+}
