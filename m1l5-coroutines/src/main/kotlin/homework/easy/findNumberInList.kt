9f35bdd8dea387cdfeeb9fb4fb574b35c8af7718
apisarevskiy <apisarevskiy@yandex.ru>
2022-04-05 17:50:35 +0300

    Корутины, домашнее задание

--- m1l5-coroutines/src/main/kotlin/homework/easy/findNumberInList.kt
+++ m1l5-coroutines/src/main/kotlin/homework/easy/findNumberInList.kt
@@ -1,6 +1,9 @@
 package homework.easy
 
-fun findNumberInList(toFind: Int, numbers: List<Int>): Int {
-    Thread.sleep(2000L)
+import kotlinx.coroutines.delay
+
+suspend fun findNumberInList(toFind: Int, numbers: List<Int>): Int {
+    //Thread.sleep(2000L)
+    delay(2000L)
     return numbers.firstOrNull { it == toFind } ?: -1
 }
--- m1l5-coroutines/src/main/kotlin/homework/easy/main.kt
+++ m1l5-coroutines/src/main/kotlin/homework/easy/main.kt
@@ -1,20 +1,45 @@
 package homework.easy
 
-fun main() {
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTimedValue
+
+suspend fun main() {
     val numbers = generateNumbers()
     val toFind = 10
     val toFindOther = 1000
 
-    val foundNumbers = listOf(
-        findNumberInList(toFind, numbers),
-        findNumberInList(toFindOther, numbers)
-    )
+    val time = measureTimeMillis {
 
-    foundNumbers.forEach {
-        if (it != -1) {
-            println("Your number $it found!")
-        } else {
-            println("Not found number $toFind || $toFindOther")
+        coroutineScope {
+
+        //val ctx = newSingleThreadContext("MyOwnThread")
+         val ctx = newFixedThreadPoolContext(2,"MyOwnThread")
+
+        val foundNumbers = listOf(
+                async(ctx) { println("Asysnc1      : I'm working in thread ${Thread.currentThread().name}")
+                        findNumberInList(toFind, numbers) },
+                async(ctx) { println("Asysnc2      : I'm working in thread ${Thread.currentThread().name}")
+                        findNumberInList(toFindOther, numbers) }
+            )
+            runCatching {
+                foundNumbers.forEach {
+                    if (it.await() != -1) {
+                        println("Your number ${it.await()} found!")
+                    } else {
+                        println("Not found number $toFind || $toFindOther")
+                    }
+                }
+            }.onFailure {
+                //println("Deffered still running? ${it.isActive}")
+               // println("Deffered is canceled? ${it.isCancelled}")
+                println("I have a problem")
+            }.getOrThrow()
+
         }
+
     }
+
+    println("Completed ib $time ms")
+
 }
